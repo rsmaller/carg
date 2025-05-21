6aebd6408931138b73e27e091ce723d08caf34a2
Ryan Smaller <ryjobobo@gmail.com>
2025-05-21 17:28:37 -0400

    Added default argument enforcement

--- args.c
+++ args.c
@@ -1,15 +1,18 @@
 #include "args.h"
+#include <libgen.h>
 
-int main(int argc, char *argv[]) {
-    int myArg = 0;
-    char myString[100] = "default";
-    float thingy = 0.1f;
-    int boolean1 = 0;
-    int boolean2 = 1;
+int main(int argc, char *argv[]) { // Example code
+    setUsageMessage("USAGE: %s -n [number] -t [string] <-b> <-c> <-z [float]>", basename(argv[0]));
+    argInit(int, myArg, NO_DEFAULT_VALUE);
+    argInit(char, myString[100], NO_DEFAULT_VALUE);
+    argInit(float, thingy, 0.1f);
+    argInit(char, boolean1, 0);
+    argInit(char, boolean2, 1);
     setFlagsFromArgs(argc, argv, "-n:%d -t:%99s -z:%f -b:bool -c:bool", &myArg, &myString, &thingy, &boolean1, &boolean2);
-    require(2, 
-        myArg > -1,              "Int 1 must be positive",
-        strcmp(myString, "yes"), "myString cannot be yes"
+    require(3, 
+        myArg > -1, "Int 1 must be positive",
+        REQUIRE_NO_DEFAULT_VALUE(myArg), NULL,
+        REQUIRE_NO_DEFAULT_VALUE(myString), NULL
     );
     printf("myInt: %d, myString: %s, float: %f, bool1: %d, bool2: %d\n", myArg, myString, thingy, boolean1, boolean2);
     return 0;
--- args.h
+++ args.h
@@ -3,7 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <assert.h>
+
+char usageString[1024] = "Please specify a usage message in your client code.";
+
+void usage() {
+    printf("%s\n", usageString);
+    exit(0);
+}
+
+#define setUsageMessage(...) do { \
+    snprintf(usageString, 1023, __VA_ARGS__);\
+    usageString[1023] = '\0';\
+} while (0)
 
 int compareArg(char *argument, char *parameter) {
     return !strcmp(argument, parameter);
@@ -35,13 +48,15 @@ void checkArgAgainstFormatter(int argc, char *argv[], int *argIndex, const char 
         flagItem = strtok_r(internalFormatter, ": ", &savePointer);
         formatterItem = strtok_r(NULL, ": ", &savePointer);
         internalFormatter = savePointer;
-        if (!flagItem) break;
+        if (!flagItem) {
+            break;
+        }
         if (!strcmp(flagItem, argv[*argIndex])) {
             flagCopierPointer = va_arg(formatterArgs, void *);
             if (!strcmp(formatterItem, "bool")) {
-                *(int *)flagCopierPointer = !*(int *)flagCopierPointer; // Flip flag from its default value. Boolean flags are expected to be ints with a default value.
+                *(char *)flagCopierPointer = !*(char *)flagCopierPointer; // Flip flag from its default value. Boolean flags are expected to be chars with a default value.
             } else {
-                sscanf(argv[(*argIndex) + 1], formatterItem, flagCopierPointer);
+                sscanf(argv[(*argIndex) + 1], formatterItem, flagCopierPointer); // If an argument is passed in that does not match its formatter, the value remains default.
             }
             break;
         }
@@ -61,9 +76,9 @@ void setFlagsFromArgs(int argc, char *argv[], const char *argFormatter, ...) {
     return;
 }
 
-void require(int assertionCount, ...) {
-    va_list args;
-    va_start(args, assertionCount);
+void require(int assertionCount, ...) { // Pass in the number of assertions, followed by sets of test cases and messages for each assertion.
+    va_list args;                       // Pass in NULL for a message to default to the usage message.
+    va_start(args, assertionCount);     // Be sure to call this after calling setFlagsFromArgs() to get data from the user. This function is designed to validate command line arguments.
     int expression;
     char *message;
     int exitFlag = 0;
@@ -71,11 +86,27 @@ void require(int assertionCount, ...) {
         expression = va_arg(args, int);
         message = va_arg(args, char *);
         if (!(expression)) {
-            printf("%s\n", message);
-            exitFlag = 1;
+            if (message) {
+                printf("%s\n", message);
+                exitFlag = 1;
+            } else {
+                usage();
+            }
         }
     }
     if (exitFlag) exit(0);
     va_end(args);
 }
 
+#define argInit(leftType, varName, value) \
+    leftType varName = value;\
+    const leftType default_##varName = value; // This macro is designed to be used with the left side of the variable declaration freed up so a "default" variable can be created from its name.
+                                              // For example, the variable char **myInt[100] = {0} should be declared as argInit(char **, myInt[100], {0});
+
+#define REQUIRE_NO_DEFAULT_VALUE(varName)\
+    memcmp(&varName, &default_##varName, sizeof(varName)) // This is designed to be used in the require() function to assert that an argument should not have a default value.
+                                                          // This is NOT designed to handle pointers to heap-allocated memory.
+                                                          // The default value macros should only be used on array types, not pointer types.
+                                                          // If you need to use heap memory, memcpy() the data into a heap memory space after fetching it from the user.
+
+#define NO_DEFAULT_VALUE {0} // For readability purposes.
