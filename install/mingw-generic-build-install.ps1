dcf2d690ae7e61457e1efaf233539f944d3b8bde
rsmaller <ryjobobo@gmail.com>
2025-08-26 16:40:00 -0400

    Switched from dlltools chain to GCC import library generation for linking client code

--- install/mingw-generic-build-install.ps1
+++ install/mingw-generic-build-install.ps1
@@ -1,11 +1,8 @@
 $mingwpath = gcc -print-search-dirs | findstr install:.
 $mingwpath = $mingwpath.Substring(9) + "include/"
-gcc -I../include -c -fpic ../src/carg_impl.c
-gcc -shared -o carg.dll carg_impl.o
-gendef carg.dll
-dlltool -d carg.def -l carg.lib -D carg.dll
+gcc -I../include -c -fpic ../src/carg_impl.c -o carg_impl.o
+gcc -shared -o carg.dll carg_impl.o "-Wl,--out-implib,carg.dll.a"
 Remove-Item carg_impl.o
-Remove-Item carg.def
 mkdir -Force "C:\Program Files\libcarg\"
 Copy-Item carg.dll "C:\Program Files\libcarg\"
 $prevPath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
