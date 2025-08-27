d7a46a8cd18862ef5625cc339b8bf3263b4b1db9
rsmaller <ryjobobo@gmail.com>
2025-08-26 23:52:03 -0400

    UNIX install script copies to local directories for libraries, and added CPack build files to gitignore

--- install/unix-generic-build-install.sh
+++ install/unix-generic-build-install.sh
@@ -2,6 +2,7 @@
 gcc -I../include -c -fpic ../src/carg_impl.c
 gcc -shared -o libcarg.so carg_impl.o
 rm carg_impl.o
-sudo cp libcarg.so /usr/lib/
-sudo mkdir -p /usr/include/carg
-sudo cp ../include/carg.h /usr/include/carg
+sudo cp libcarg.so /usr/local/lib/
+sudo mkdir -p /usr/local/include/carg
+sudo cp ../include/carg.h /usr/local/include/carg
+sudo ldconfig
